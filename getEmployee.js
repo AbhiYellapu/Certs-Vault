ea3c31ac7dbdaf1baafd82fd46f278806b56a820
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-04-25 16:43:31 +0530

    Add files via upload

---
+++ getEmployee.js
@@ -0,0 +1,12 @@
+// Get all Employeee Details
+
+import { connectToDatabase } from "./dbconnection.js";
+
+export async function getEmployeeDetails(db, employeeID) {
+        let table = 'Employee';
+        let query = `SELECT * FROM ${table} WHERE EmployeeID = ?;`
+        return db.get(query, employeeID);
+}
+// const db = await connectToDatabase();
+// const employee = await getEmployeeDetails(db, "E0001");
+// console.log(employee);
