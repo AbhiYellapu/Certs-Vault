2e8f24071fcabbd14d2962c89e180aae6d02f025
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-04-26 23:05:57 +0530

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -6,7 +6,7 @@ import { open } from 'sqlite';
 export async function connectToDatabase() {
     try {
         let db = await open({
-            filename: './test.db',
+            filename: './CertVault.db',
             driver: sqlite3.Database
         });
         return db;
