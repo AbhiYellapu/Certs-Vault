ccc541a5e8f2c32a724877d77d156c7150d75011
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-04-26 23:35:14 +0530

    Update apis.js

--- apis.js
+++ apis.js
@@ -8,18 +8,18 @@ const app = express();
 app.use(express.json());
 const db = await connectToDatabase();
 
-app.get('/api/:empID/certs', async function (request, response) {
-    const queryParamsKeys = Object.keys(request.query);
-    // let empID = request.query.empID;
-    let empID = request.params.empID;
-    let sortOption = request.query.sortOption || "CertName";
-    let sortBy = request.query.sortBy || "ASC";
-
+app.get('/api/:empID/certs', async function (request, response) {    
     try {
+        const queryParamsKeys = Object.keys(request.query);
+        let empID = request.params.empID;
+        let sortOption = request.query.sortOption || "CertName";
+        let sortBy = request.query.sortBy || "ASC";
+
         let selectQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL 
         FROM EmployeeCertificates, Certificate 
         WHERE EmployeeCertificates.CertID = Certificate.CertID AND EmployeeCertificates.EmployeeID = ? 
         ORDER BY ${sortOption} ${sortBy};`
+        
         const certs = await db.all(selectQuery, empID);
         response.send({ Certificates: certs });
     } catch (error) {
@@ -29,23 +29,18 @@ app.get('/api/:empID/certs', async function (request, response) {
 
 app.put('/api/:empID/insertCert', async function (request, response) {
     let cert;
-    // let empID = request.query.empID;
     let empID = request.params.empID;
     if (request.get('Content-Type') !== 'application/json') {
         response.status(400).send("Invalid JSON");
     } else {
         cert = request.body;
         try {
-            // let certIDQuery = `SELECT CertID FROM Certificate WHERE CertName = '${cert.Certificate.CertName}'`;
-            // let certID = (await db.get(certIDQuery)).CertID;
             let insertQuery = `INSERT INTO EmployeeCertificates (CertID, EmployeeID, IssueDate, ExpireDate, CredentialID, CredentialURL) VALUES (?, ?, ?, ?, ?, ?);`
-            let affectedRows = await db.run(insertQuery, (await getCertID(cert.Certificate.CertName)), empID, cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL);
+            let affectedRow = await db.run(insertQuery, (await getCertID(cert.Certificate.CertName)), empID, cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL);
 
-            let affectedRowQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL
-            FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
-            WHERE EmployeeCertificates.rowid = ?`;
-            let insertedCertificate = { Certificate: (await db.get(affectedRowQuery, affectedRows.lastID)) };
-            response.send(insertedCertificate);
+            let insertedCertificate = await getAffectedRow(affectedRow.lastID);
+            response.send({ InsertedCertificate: insertedCertificate, status: "Certificate inserted successfully" });
+
         } catch (error) {
             response.status(500).send(error);
             console.log(error);
@@ -54,18 +49,17 @@ app.put('/api/:empID/insertCert', async function (request, response) {
 });
 
 app.post('/api/:empID/certs/editCert', async function (request, response) {
-    let empID = request.params.empID;
-    let certID = request.query.certID;
-    let cert = request.body;
     try {
-        let updateQuery = `UPDATE EmployeeCertificates SET CertID = ?, IssueDate = ?,  ExpireDate = ?, CredentialID = ?, CredentialURL = ? WHERE EmployeeID = ? AND CertID = ?;`;
-        console.log(updateQuery, (await getCertID(cert.Certificate.CertName)), cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL);
-        let affectedRow = await db.run(updateQuery, (await getCertID(cert.Certificate.CertName)), cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL, empID, certID);
+        let empID = request.params.empID;
+        let certID = request.query.certID;
+        let cert = request.body;
+
+        let editQuery = `UPDATE EmployeeCertificates SET CertID = ?, IssueDate = ?,  ExpireDate = ?, CredentialID = ?, CredentialURL = ? WHERE EmployeeID = ? AND CertID = ?;`;
+        let affectedRow = await db.run(editQuery, (await getCertID(cert.Certificate.CertName)), cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL, empID, certID);
         
-        console.log(affectedRow.changes);
-        let updatedCertificate = await getCertificate(empID, certID);
         if (affectedRow.changes == 1) {
-            response.send(updatedCertificate);
+            let editedCertificate = await getCertificate(empID, certID);
+            response.send({ EditedCertificate: editedCertificate, status: "Certificate edited successfully" });
         } else {
             response.status(500).send({ status: 'Failed to edit the certificate' });
         }
@@ -76,15 +70,16 @@ app.post('/api/:empID/certs/editCert', async function (request, response) {
 });
 
 app.delete('/api/:empID/certs', async function (request, response) {
-    let empID = request.params.empID;
-    let certID = request.query.certID;
     try {
+        let empID = request.params.empID;
+        let certID = request.query.certID;
+
         let deleteQuery = `DELETE FROM EmployeeCertificates WHERE CertID = ? AND EmployeeID = ?;`;
         let deletedCertificate = await getCertificate(empID, certID);
         const affectedRow = await db.run(deleteQuery, certID, empID);
 
         if (affectedRow.changes == 1) {
-            response.send(deletedCertificate);
+            response.send({ DeletedCertificate: deletedCertificate, status: "Certificate deleted successfully" });
         } else {
             response.status(500).send({ status: 'Failed to delete the certificate' });
         }
@@ -99,13 +94,13 @@ app.get('/api/:user/searchCert', async function (request, response) {
     const queryParamsKeys = Object.keys(request.query);
     let optionKey = queryParamsKeys[0];
     let searchOption = request.query[optionKey];
-    const certs = await getCert(db, empID, searchOption);
+
+    // const certs = await getCert(empID, searchOption);
     response.send(certs);
 });
 
 app.listen(port, function () {
-
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port: ${port}.`);
 });
 
 async function getCertID(certName) {
@@ -118,12 +113,12 @@ async function getCertID(certName) {
     }
 }
 
-async function getAffectedCertificate(affectedRowID) {
+async function getAffectedRow(affectedRowID) {
     let affectedRowQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL
     FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
     WHERE EmployeeCertificates.rowid = ?`;
     try {
-        return { Certificate: (await db.get(affectedRowQuery, affectedRowID)) };
+        return (await db.get(affectedRowQuery, affectedRowID));
     } catch (error) {
         return error;
     }
@@ -134,7 +129,7 @@ async function getCertificate(empID, certID) {
     FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
     WHERE EmployeeCertificates.EmployeeID = ? AND EmployeeCertificates.CertID = ?`;
     try {
-        return { Certificate: (await db.get(query, empID, certID)) };
+        return (await db.get(query, empID, certID));
     } catch (error) {
         return error;
     }
