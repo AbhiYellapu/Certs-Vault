07852ad96c6697a4072706bd7870da74ba92c9f9
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-04-26 23:03:50 +0530

    Update apis.js

--- apis.js
+++ apis.js
@@ -1,53 +1,141 @@
 // Certification api's
 
-import express from 'express'
-import sqlite3 from 'sqlite3'
-import {connectToDatabase} from './dbconnection.js'
-import {getEmployeeDetails} from './getEmployee.js'
+import express, { query } from 'express'
+import { connectToDatabase } from './dbconnection.js'
 
 const port = 3000;
 const app = express();
-
+app.use(express.json());
 const db = await connectToDatabase();
 
-app.get('/api/me/certs', async function (request, response) {
+app.get('/api/:empID/certs', async function (request, response) {
     const queryParamsKeys = Object.keys(request.query);
-    let empID  = request.query.EmployeeID;
-    let optionKey = queryParamsKeys[1];
+    // let empID = request.query.empID;
+    let empID = request.params.empID;
+    let sortOption = request.query.sortOption || "CertName";
+    let sortBy = request.query.sortBy || "ASC";
 
-    if (optionKey == "Sort") {
-        let sortOption = request.query.Sort;
-        // const certs = await getAllCerts(db, empID, sortOption);
-        // response.send(certs);
-    } else {
-        let searchOption = request.query[optionKey];
-        // const certs = await getCert(db, empID, searchOption);
-        // response.send(certs);
+    try {
+        let selectQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL 
+        FROM EmployeeCertificates, Certificate 
+        WHERE EmployeeCertificates.CertID = Certificate.CertID AND EmployeeCertificates.EmployeeID = ? 
+        ORDER BY ${sortOption} ${sortBy};`
+        const certs = await db.all(selectQuery, empID);
+        response.send({ Certificates: certs });
+    } catch (error) {
+        response.send(error);
     }
 });
 
-app.get('/api/account', async function (request, response) {
-    let empID  = request.query.EmployeeID;
-    const employee = await getEmployeeDetails(db, empID);
-    response.send(employee);
-})
+app.put('/api/:empID/insertCert', async function (request, response) {
+    let cert;
+    // let empID = request.query.empID;
+    let empID = request.params.empID;
+    if (request.get('Content-Type') !== 'application/json') {
+        response.status(400).send("Invalid JSON");
+    } else {
+        cert = request.body;
+        try {
+            // let certIDQuery = `SELECT CertID FROM Certificate WHERE CertName = '${cert.Certificate.CertName}'`;
+            // let certID = (await db.get(certIDQuery)).CertID;
+            let insertQuery = `INSERT INTO EmployeeCertificates (CertID, EmployeeID, IssueDate, ExpireDate, CredentialID, CredentialURL) VALUES (?, ?, ?, ?, ?, ?);`
+            let affectedRows = await db.run(insertQuery, (await getCertID(cert.Certificate.CertName)), empID, cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL);
 
-api.post('/api/me/insertCert', function (request, response) {
-    response.send("");
+            let affectedRowQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL
+            FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
+            WHERE EmployeeCertificates.rowid = ?`;
+            let insertedCertificate = { Certificate: (await db.get(affectedRowQuery, affectedRows.lastID)) };
+            response.send(insertedCertificate);
+        } catch (error) {
+            response.status(500).send(error);
+            console.log(error);
+        }
+    }
 });
 
-api.put('/api/me/updateCert', function (request, response) {
-    response.send("");
+app.post('/api/:empID/certs/editCert', async function (request, response) {
+    let empID = request.params.empID;
+    let certID = request.query.certID;
+    let cert = request.body;
+    try {
+        let updateQuery = `UPDATE EmployeeCertificates SET CertID = ?, IssueDate = ?,  ExpireDate = ?, CredentialID = ?, CredentialURL = ? WHERE EmployeeID = ? AND CertID = ?;`;
+        console.log(updateQuery, (await getCertID(cert.Certificate.CertName)), cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL);
+        let affectedRow = await db.run(updateQuery, (await getCertID(cert.Certificate.CertName)), cert.Certificate.IssueDate, cert.Certificate.ExpireDate, cert.Certificate.CredentialID, cert.Certificate.CredentialURL, empID, certID);
+        
+        console.log(affectedRow.changes);
+        let updatedCertificate = await getCertificate(empID, certID);
+        if (affectedRow.changes == 1) {
+            response.send(updatedCertificate);
+        } else {
+            response.status(500).send({ status: 'Failed to edit the certificate' });
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).send(error);
+    }
 });
 
-api.delete('/api/me/updateCert', function (request, response) {
-    response.send("");
+app.delete('/api/:empID/certs', async function (request, response) {
+    let empID = request.params.empID;
+    let certID = request.query.certID;
+    try {
+        let deleteQuery = `DELETE FROM EmployeeCertificates WHERE CertID = ? AND EmployeeID = ?;`;
+        let deletedCertificate = await getCertificate(empID, certID);
+        const affectedRow = await db.run(deleteQuery, certID, empID);
+
+        if (affectedRow.changes == 1) {
+            response.send(deletedCertificate);
+        } else {
+            response.status(500).send({ status: 'Failed to delete the certificate' });
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).send(error);
+    }
 });
 
-api.get('api/loginCredentials', function (request, response) {
-    response.send("");
-})
+app.get('/api/:user/searchCert', async function (request, response) {
+    let empID = request.params.empID;
+    const queryParamsKeys = Object.keys(request.query);
+    let optionKey = queryParamsKeys[0];
+    let searchOption = request.query[optionKey];
+    const certs = await getCert(db, empID, searchOption);
+    response.send(certs);
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
+
     console.log(`Server is running on port ${port}`);
 });
+
+async function getCertID(certName) {
+    let certIDQuery = `SELECT CertID FROM Certificate WHERE CertName = '${certName}'`;
+    try {
+        return (await db.get(certIDQuery)).CertID;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+async function getAffectedCertificate(affectedRowID) {
+    let affectedRowQuery = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL
+    FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
+    WHERE EmployeeCertificates.rowid = ?`;
+    try {
+        return { Certificate: (await db.get(affectedRowQuery, affectedRowID)) };
+    } catch (error) {
+        return error;
+    }
+}
+
+async function getCertificate(empID, certID) {
+    let query = `SELECT Certificate.CertName, Certificate.OrganizationName, EmployeeCertificates.IssueDate, EmployeeCertificates.ExpireDate, EmployeeCertificates.CredentialID, EmployeeCertificates.CredentialURL
+    FROM Certificate JOIN EmployeeCertificates ON Certificate.CertID = EmployeeCertificates.CertID
+    WHERE EmployeeCertificates.EmployeeID = ? AND EmployeeCertificates.CertID = ?`;
+    try {
+        return { Certificate: (await db.get(query, empID, certID)) };
+    } catch (error) {
+        return error;
+    }
+}
